b443d24180637e7c3b151c3fa9bff2b7ef77db7b
lhernandez19 <diegotapia1987@gmail.com>
2022-02-28 17:10:30 -0600

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      This is the <strong>HOME</strong> page
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div>
+      This is the <strong>PRODUCTS</strong> page
+    </div>
+  );
+}
+
+export default Products;
